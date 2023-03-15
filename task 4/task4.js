c0777cac1822526add1979052a09451b9745b490
raviranjan20289 <diler20289@gmail.com>
2023-03-15 23:00:26 +0530

    fourth task

---
+++ task 4/task4.js
@@ -0,0 +1,8 @@
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.backgroundColor = "green";
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = 'bold';
+}
