acf2a1629045d161d85bb89c41a930149326f8ce
raviranjan20289 <diler20289@gmail.com>
2023-03-16 13:59:15 +0530

    six first task

---
+++ task 6/task6.js
@@ -0,0 +1,15 @@
+var secondItem = document.querySelector("#items li:nth-child(2)");
+secondItem.style.backgroundColor = "green";
+
+
+var thirdItem = document.querySelector("#items li:nth-child(3)");
+thirdItem.style.visibility = "hidden";
+
+
+// document.querySelector("#items li:nth-child(2)").style.color = "green";
+
+
+// const oddItems = document.querySelectorAll("#items li:nth-child(odd)");
+// for (let i = 0; i < oddItems.length; i++) {
+//   oddItems[i].style.backgroundColor = "green";
+// }
